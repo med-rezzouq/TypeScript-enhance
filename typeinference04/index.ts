b2187b1fcd48a32d3953dc668dc252f07ec1e243
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-24 23:40:43 +0100

    (typeinference means typescript can guess the types from assignement only):

---
+++ typeinference04/index.ts
@@ -0,0 +1,15 @@
+//type inference is when type script guess the type from the variables
+
+//here the types are (string | number | boolean) only 
+let names = ['joe', 'jen', 10, true];
+
+
+//when we dont assingn a value to a variable we can reassign it example
+let annee
+annee = 2022
+annee = 'abcd' //that works
+
+// but if we do like this
+let mois = 11
+// mois='abcd'  // this is impossible because number type already assigned
+
