877de1e4c070a6c9a818284945da43ef95ad5c45
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-25 15:29:58 +0100

    (enable declaration in tsconfig to generate a file have all our declarations like roadmap, also we talk about how to return number instead of string where we do tofixed or other functions):
    we add + after return

---
+++ project07/src/index.ts
@@ -0,0 +1,9 @@
+function calTotalPrice(price: number):number {
+   
+
+    return +(price + (price * 0.18)).toFixed(1);
+}
+
+const priceOfToy = calTotalPrice(100);
+const discountPrice = priceOfToy / 2;
+calTotalPrice(discountPrice);
